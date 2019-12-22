30155245136edbc97ff178e273eb35fd7d5f8d75
Whit Jackman <whitjackman@gmail.com>
2019-12-22 16:16:03 -0500

    Added new terrain sprites and modified Tile and Assets class to load and use added sprites

--- PokemonRPG/com/game/Entities/Tile.java
+++ PokemonRPG/com/game/Entities/Tile.java
@@ -15,22 +15,31 @@ public class Tile extends Entity
   private static Map<String, BufferedImage> TILE_IMAGES =
       new HashMap<String, BufferedImage>()
       {{
-        put("grass",                Assets.spriteTileGrass);
-        put("dirt",                 Assets.spriteTileDirt);
-        put("water",                Assets.spriteTileWater);
-        put("weed",                 Assets.spriteTileWeed);
-        put("house_roof_top",       Assets.spriteHouse1_roof_top);
-        put("house_roof_bottom",    Assets.spriteHouse1_roof_bottom);
-        put("houseTop_left",        Assets.spriteHouse1_houseTop_left);
-        put("houseTop_midLeft",     Assets.spriteHouse1_houseTop_midLeft);
-        put("houseTop_midRight",    Assets.spriteHouse1_houseTop_midRight);
-        put("houseTop_right",       Assets.spriteHouse1_houseTop_right);
-        put("houseBottom_left",     Assets.spriteHouse1_houseBottom_left);
-        put("houseBottom_midLeft",  Assets.spriteHouse1_houseBottom_midLeft);
-        put("houseBottom_midRight", Assets.spriteHouse1_houseBottom_midRight);
-        put("houseBottom_right",    Assets.spriteHouse1_houseBottom_right);
-      }};
-      
+        put("grass",                   Assets.spriteTileGrass);
+        put("dirt",                    Assets.spriteTileDirt);
+        put("water",                   Assets.spriteTileWater);
+        put("weed",                    Assets.spriteTileWeed);
+        put("house_roof_top",          Assets.spriteHouse1_roof_top);
+        put("house_roof_bottom",       Assets.spriteHouse1_roof_bottom);
+        put("houseTop_left",           Assets.spriteHouse1_houseTop_left);
+        put("houseTop_midLeft",        Assets.spriteHouse1_houseTop_midLeft);
+        put("houseTop_midRight",       Assets.spriteHouse1_houseTop_midRight);
+        put("houseTop_right",          Assets.spriteHouse1_houseTop_right);
+        put("houseBottom_left",        Assets.spriteHouse1_houseBottom_left);
+        put("houseBottom_midLeft",     Assets.spriteHouse1_houseBottom_midLeft);
+        put("houseBottom_midRight",    Assets.spriteHouse1_houseBottom_midRight);
+        put("houseBottom_right",       Assets.spriteHouse1_houseBottom_right);
+        put("treeBottom_aloneLeft",    Assets.spriteTree_bottomAloneLeft);
+        put("treeBottom_aloneRight",   Assets.spriteTree_bottomAloneRight);
+        put("treeBottom_overlapLeft",  Assets.spriteTree_bottomOverlapLeft);
+        put("treeBottom_overlapRight", Assets.spriteTree_bottomOverlapRight);
+        put("treeMiddleLeft",          Assets.spriteTree_middleLeft);
+        put("treeMiddleRight",         Assets.spriteTree_middleRight);
+        put("treeTop_aloneLeft",       Assets.spriteTree_topAloneLeft);
+        put("treeTop_aloneRight",      Assets.spriteTree_topAloneRight);
+        
+        put("No Image",                Assets.noImage);
+      }};    
   
   private Integer              oRow         = null;
   private Integer              oCol         = null;
